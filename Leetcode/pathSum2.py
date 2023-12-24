2fb82c2ccb56b5e7b5cb5a8358e45b1a8f396ac9
sajidmahmud69 <sajidmahmud136@yahoo.com>
2023-12-24 17:57:06 -0500

    add tree solutions

---
+++ Leetcode/pathSum2.py
@@ -0,0 +1,33 @@
+from tree import Tree, buildTree, inOrder
+
+
+def pathSum2(root, targetSum):
+    def dfs(root, targetSum, result = [], tmp = []):
+        if not root:
+            return
+
+        tmp.append(root.val)
+
+        if targetSum == root.val and root.left is None and root.right is None:
+            result.append(tmp)
+        
+        dfs(root.left, targetSum - root.val, result, tmp[:])
+        dfs(root.right, targetSum - root.val, result, tmp[:])
+
+        return result
+
+    return dfs(root, targetSum)
+
+
+if __name__ == '__main__':
+
+    arr = [5,4,8,11,None,13,4,7,2,None,None,5,1]
+    targetSum = 22
+    arr = [1, 2]
+    targetSum = 1
+    root = buildTree(arr, 0, len(arr))
+    print(pathSum2(root, targetSum))
+        
+
+        
+        
